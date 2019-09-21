36f9c2ab6219012ef3c1ade68ca331ed878dfbb4
Dievas3220 <eimisklim@gmail.com>
2019-09-21 18:37:08 +0300

    doctor chose added

--- doctorchooseclient.php
+++ doctorchooseclient.php
@@ -3,13 +3,11 @@ require('DatabaseConnection.php');
 
 $databaseConnection = new DatabaseConnection();
 $conn = $databaseConnection->connect();
-
-$sql = "USE doctor";
-$conn->query($sql);
 $ID = $_GET['client'];
 echo($ID);
-$sql = "UPDATE client SET STATUS = 'IN progress' WHERE ID = " . $ID;
+$doctorid = $_GET['DoctorID'];
+$sql = "UPDATE client SET STATUS = 'Done' WHERE Status = 'In progress' AND DoctorID = " . $doctorid;
 $result = $conn->query($sql);
-
+$sql = "UPDATE client SET STATUS = 'In progress' WHERE ID = " . $ID ." AND DoctorID = " . $doctorid;
+$result = $conn->query($sql);
 $conn->close();
-
