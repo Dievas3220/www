08a235cf9ac40c487a0594adecadac647a221a5b
Dievas3220 <eimisklim@gmail.com>
2019-09-21 17:35:09 +0300

    database connection added

---
+++ DatabaseConnection.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class DatabaseConnection
+{
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = "mysql";
+
+    public function connect()
+    {
+        $conn = new mysqli($this->servername, $this->username, $this->password);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+
+        }
+
+        return $conn;
+}
+}
