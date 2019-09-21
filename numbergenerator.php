430a86c3edebb153baf5ca8e20d58b266c0e273c
Dievas3220 <eimisklim@gmail.com>
2019-09-21 18:40:43 +0300

    doctor choose client updated

--- numbergenerator.php
+++ numbergenerator.php
@@ -1,22 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "mysql";
+require('DatabaseConnection.php');
 
-$conn = new mysqli($servername, $username, $password);
+$databaseConnection = new DatabaseConnection();
+$conn = $databaseConnection->connect();
+$doctor = $_GET["doctor"];
+$name = $_GET["firstname"];
 
-if  ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-$doctor=$_GET["doctor"];
-$name=$_GET["firstname"];
-$sql = "USE doctor";
-$conn->query($sql);
 $sql = "INSERT INTO CLIENT (`NAME`, `DoctorID`, `STATUS`) VALUES ('" . $name . "','" . $doctor . "', 'to do')";
-if ($conn->query($sql) === True) {
+if ($conn->query($sql) === true) {
     $last_id = $conn->insert_id;
     echo "Klientas sekmingai uzregistruotas. Jusu numeris yra: " . $last_id;
-}  else {
+} else {
     echo "Ivyko klaida, kreipkites telefonu.";
 }
 
