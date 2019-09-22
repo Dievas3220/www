e9a03392bae7ba81dc005c93d931002beb7485cf
Dievas3220 <55597294+Dievas3220@users.noreply.github.com>
2019-09-22 12:35:07 +0300

    status is set in lowercase

--- doctorpage.php
+++ doctorpage.php
@@ -4,7 +4,7 @@ require('DatabaseConnection.php');
 $databaseConnection = new DatabaseConnection();
 $conn = $databaseConnection->connect();
 
-$sql = "SELECT * FROM client WHERE status = 'TO DO'";
+$sql = "SELECT * FROM client WHERE status = 'to do'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
