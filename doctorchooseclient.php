f0d5cabf4ae907da71dbbdeb24e3f6e56e8effa0
Dievas3220 <55597294+Dievas3220@users.noreply.github.com>
2019-09-22 18:06:19 +0300

    variable rename

--- doctorchooseclient.php
+++ doctorchooseclient.php
@@ -3,11 +3,11 @@ require('DatabaseConnection.php');
 
 $databaseConnection = new DatabaseConnection();
 $conn = $databaseConnection->connect();
-$ID = $_GET['client'];
+$clientId = $_GET['client'];
 $doctorId = $_GET['doctorId'];
-echo "Priimtas nr. " . $ID . "<a href='doctorpage.php'> Gryžti į sarašą </a>";
+echo "Priimtas nr. " . $clientId . "<a href='doctorpage.php'> Gryžti į sarašą </a>";
 $sql = "UPDATE client SET status = 'Done' WHERE status = 'In progress' AND doctor_id = " . $doctorId;
 $result = $conn->query($sql);
-$sql = "UPDATE client SET status = 'In progress' WHERE id = " . $ID ." AND doctor_id = " . $doctorId;
+$sql = "UPDATE client SET status = 'In progress' WHERE id = " . $clientId ." AND doctor_id = " . $doctorId;
 $result = $conn->query($sql);
 $conn->close();
