e8dd363d066ea5ebdedad7158eeb724df822f028
Dievas3220 <eimisklim@gmail.com>
2019-09-22 12:01:22 +0300

    webpage look fixed CLIENT -> client

--- doctorpage.php
+++ doctorpage.php
@@ -6,11 +6,11 @@ $conn = $databaseConnection->connect();
 
 $sql = "USE doctor";
 $conn->query($sql);
-$sql = "SELECT * FROM CLIENT WHERE STATUS = 'TO DO'";
+$sql = "SELECT * FROM client WHERE STATUS = 'TO DO'";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo "id: " . $row["ID"] . "- Name: " . $row["name"] . "
+        echo "ID: " . $row["ID"] . "- Vardas: " . $row["name"] . "
         <br>";
     }
 } else {
