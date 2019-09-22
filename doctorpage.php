44323be79480e70ab342028952f3b0976098e5f0
Dievas3220 <55597294+Dievas3220@users.noreply.github.com>
2019-09-22 17:55:17 +0300

    Doctor choice added.

--- doctorpage.php
+++ doctorpage.php
@@ -3,8 +3,8 @@ require('DatabaseConnection.php');
 
 $databaseConnection = new DatabaseConnection();
 $conn = $databaseConnection->connect();
-
-$sql = "SELECT * FROM client WHERE status = 'to do'";
+$doctorId = $_GET['doctorId'];
+$sql = "SELECT * FROM client WHERE status = 'to do' AND doctor_id = " . $doctorId;
 $result = $conn->query($sql);
 
 $conn->close();
@@ -17,17 +17,30 @@ echo
     <title>Daktaro puslapis</title>
 </head>
 <body>";
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo $row["id"] . " " . $row["name"] . "<br>";
-    }
+
+if (empty($doctorId)) {
+    echo
+    "<form action='doctorpage.php'>
+    <input type='radio' name='doctorId' value='1'> Dr. Vytas<br>
+    <input type='radio' name='doctorId' value='2'> Dr. Ona<br>
+    <input type='radio' name='doctorId' value='3'> Dr. Kazys<br>
+    <input type='submit' value='Submit' method='get'>
+
+</form>
+";
 } else {
-    echo "0 results";
-}
-echo "<form action='doctorchooseclient.php'>
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo $row["id"] . " " . $row["name"] . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    echo "<form action='doctorchooseclient.php'>
    Kviesti pacienta ID:<br>
     <input type='text' name='client'><br>
     <input type='submit' value='Submit' method='get'>
 </form>
 </body>
 </html>";
+}
