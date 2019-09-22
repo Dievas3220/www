651ff700c083bc6c1e884523230d96977195c706
Dievas3220 <eimisklim@gmail.com>
2019-09-22 12:02:07 +0300

    language fixed CLIENT -> client

--- numbergenerator.php
+++ numbergenerator.php
@@ -6,12 +6,12 @@ $conn = $databaseConnection->connect();
 $doctor = $_GET["doctor"];
 $name = $_GET["firstname"];
 
-$sql = "INSERT INTO CLIENT (`NAME`, `DoctorID`, `STATUS`) VALUES ('" . $name . "','" . $doctor . "', 'to do')";
+$sql = "INSERT INTO client (`NAME`, `DoctorID`, `STATUS`) VALUES ('" . $name . "','" . $doctor . "', 'to do')";
 if ($conn->query($sql) === true) {
     $last_id = $conn->insert_id;
-    echo "Klientas sekmingai uzregistruotas. Jusu numeris yra: " . $last_id;
+    echo "Klientas sėkmingai užregistruotas. Jūsų numeris yra: " . $last_id;
 } else {
-    echo "Ivyko klaida, kreipkites telefonu.";
+    echo "Ivyko klaida, kreipkitės telefonu.";
 }
 
 $conn->close();
