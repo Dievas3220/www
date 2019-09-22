1fb8d2f494ef775e8fb7a397a812eda40c61861c
Dievas3220 <55597294+Dievas3220@users.noreply.github.com>
2019-09-22 18:48:27 +0300

    use index.php instead of index.html

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo "
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <title>Ligoninės priimamasis</title>
+</head>
+<body>";
+include 'links.php';
+echo "
+</body>
+</html>
+";
