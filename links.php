c8c2794a9469b9d6e48ac80b14b31d0a987da543
Dievas3220 <55597294+Dievas3220@users.noreply.github.com>
2019-09-22 18:46:26 +0300

    Create links.php

---
+++ links.php
@@ -0,0 +1,7 @@
+<?php
+echo "
+<a href='ineedadoctor.php'>Registruotis pas daktarą</a>
+<a href='doctorpage.php'>Daktaro puslapis</a>
+<a href='scoreboard.php'>Laukiamojo švieslentė</a>
+<br><br>
+";
